57e8ea3b1354b1cff2b4ef03a26b4306cc60288a
Dave Crogan <david.crogan@yahoo.com>
2021-03-19 08:13:32 -0700

    Add files via upload

---
+++ ESP32EMQMQTTSecure/ESP32EMQMQTTSecure.ino
@@ -0,0 +1,151 @@
+/*
+ *  Program will connect and loop 3 times before disconnecting
+ *  credentials.h must be update with your Wifi information. File is in same folder
+*/
+
+#include <WiFiClientSecure.h>
+#include <credentials.h>
+#include <PubSubClient.h>
+
+
+// update these values in the credentials.h file
+const char* ssid = mySSID;
+const char* password = myPASSWORD;
+
+// Choose MQTT Server URL or IP
+const char*  mqtt_server = "broker.emqx.io";
+const int mqtt_port = 8883;   
+String client_id = "ESP32";
+const char *mqtt_username = "emqx";          
+const char *mqtt_password = "public";       
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (200)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+
+// Public Key from:
+// https://www.emqx.io/mqtt/public-mqtt5-broker
+const char* root_ca = \ 
+"-----BEGIN CERTIFICATE-----\n" \ 
+"MIIF3jCCA8agAwIBAgIQAf1tMPyjylGoG7xkDjUDLTANBgkqhkiG9w0BAQwFADCB\n" \
+"iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl\n" \
+"cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV\n" \
+"BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTAw\n" \
+"MjAxMDAwMDAwWhcNMzgwMTE4MjM1OTU5WjCBiDELMAkGA1UEBhMCVVMxEzARBgNV\n" \
+"BAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0plcnNleSBDaXR5MR4wHAYDVQQKExVU\n" \
+"aGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNVBAMTJVVTRVJUcnVzdCBSU0EgQ2Vy\n" \
+"dGlmaWNhdGlvbiBBdXRob3JpdHkwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIK\n" \
+"AoICAQCAEmUXNg7D2wiz0KxXDXbtzSfTTK1Qg2HiqiBNCS1kCdzOiZ/MPans9s/B\n" \
+"3PHTsdZ7NygRK0faOca8Ohm0X6a9fZ2jY0K2dvKpOyuR+OJv0OwWIJAJPuLodMkY\n" \
+"tJHUYmTbf6MG8YgYapAiPLz+E/CHFHv25B+O1ORRxhFnRghRy4YUVD+8M/5+bJz/\n" \
+"Fp0YvVGONaanZshyZ9shZrHUm3gDwFA66Mzw3LyeTP6vBZY1H1dat//O+T23LLb2\n" \
+"VN3I5xI6Ta5MirdcmrS3ID3KfyI0rn47aGYBROcBTkZTmzNg95S+UzeQc0PzMsNT\n" \
+"79uq/nROacdrjGCT3sTHDN/hMq7MkztReJVni+49Vv4M0GkPGw/zJSZrM233bkf6\n" \
+"c0Plfg6lZrEpfDKEY1WJxA3Bk1QwGROs0303p+tdOmw1XNtB1xLaqUkL39iAigmT\n" \
+"Yo61Zs8liM2EuLE/pDkP2QKe6xJMlXzzawWpXhaDzLhn4ugTncxbgtNMs+1b/97l\n" \
+"c6wjOy0AvzVVdAlJ2ElYGn+SNuZRkg7zJn0cTRe8yexDJtC/QV9AqURE9JnnV4ee\n" \
+"UB9XVKg+/XRjL7FQZQnmWEIuQxpMtPAlR1n6BB6T1CZGSlCBst6+eLf8ZxXhyVeE\n" \
+"Hg9j1uliutZfVS7qXMYoCAQlObgOK6nyTJccBz8NUvXt7y+CDwIDAQABo0IwQDAd\n" \
+"BgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/BAQDAgEGMA8G\n" \
+"A1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEMBQADggIBAFzUfA3P9wF9QZllDHPF\n" \
+"Up/L+M+ZBn8b2kMVn54CVVeWFPFSPCeHlCjtHzoBN6J2/FNQwISbxmtOuowhT6KO\n" \
+"VWKR82kV2LyI48SqC/3vqOlLVSoGIG1VeCkZ7l8wXEskEVX/JJpuXior7gtNn3/3\n" \
+"ATiUFJVDBwn7YKnuHKsSjKCaXqeYalltiz8I+8jRRa8YFWSQEg9zKC7F4iRO/Fjs\n" \
+"8PRF/iKz6y+O0tlFYQXBl2+odnKPi4w2r78NBc5xjeambx9spnFixdjQg3IM8WcR\n" \
+"iQycE0xyNN+81XHfqnHd4blsjDwSXWXavVcStkNr/+XeTWYRUc+ZruwXtuhxkYze\n" \
+"Sf7dNXGiFSeUHM9h4ya7b6NnJSFd5t0dCy5oGzuCr+yDZ4XUmFF0sbmZgIn/f3gZ\n" \
+"XHlKYC6SQK5MNyosycdiyA5d9zZbyuAlJQG03RoHnHcAP9Dc1ew91Pq7P8yF1m9/\n" \
+"qS3fuQL39ZeatTXaw2ewh0qpKJ4jjv9cJ2vhsE/zB+4ALtRZh8tSQZXq9EfX7mRB\n" \
+"VXyNWQKV3WKdwrnuWih0hKWbt5DHDAff9Yk2dDLWKMGwsAvgnEzDHNb842m1R0aB\n" \
+"L6KCq9NjRHDEjf8tM7qtj3u1cIiuPhnPQCjY/MiQu12ZIvVS5ljFH4gxQ+6IHdfG\n" \
+"jjxDah2nGN59PRbxYvnKkKj9\n" \
+"-----END CERTIFICATE-----\n" ;
+
+const char *topic = "testtopic/1";
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  //Turn feature off to save power later
+  if ((char)payload[0] == '1') {
+    
+    digitalWrite(2, LOW);   // Turn the LED on for testing.
+
+  } else {
+    digitalWrite(2, HIGH);  // Turn the LED off by making the voltage HIGH.
+  }
+}
+
+WiFiClientSecure wificlient;
+PubSubClient client(mqtt_server, mqtt_port, callback, wificlient);
+
+
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    // wait 1 second for re-trying
+
+    delay(1000);
+  }
+
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+
+  wificlient.setCACert(root_ca);
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+//For ports 8883 and 8884
+  Serial.println("\nStarting connection to server...");
+  if (!wificlient.connect(mqtt_server, mqtt_port)) {
+    Serial.println("Connection failed!");
+  }
+  else {
+    Serial.println("Connected to server!");
+    wificlient.println();
+
+      while(wificlient.connect(mqtt_server, mqtt_port)) {
+        
+        Serial.println("I am connected");
+
+        client.publish(topic, "hello world");
+        delay(100);
+        client.subscribe(topic);
+
+        delay(1000);
+        Serial.println("end of loop  ");
+        }
+
+    wificlient.stop();
+  }
+  
+  Serial.println("End of program");
+
+ 
+
+}
+
+void loop() {
+  // do nothing
+
+}
---
+++ ESP32EMQMQTTSecure/credentials.h
@@ -0,0 +1,6 @@
+
+#define CREDENTIALS 1
+
+// Enter your Wifi connection information
+#define mySSID "................"
+#define myPASSWORD "............"
